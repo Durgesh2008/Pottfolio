b0b959039015b8b568051ffea8d6cb076ddb553b
Durgesh Kumar <durgeshksm2000@gmail.com>
2023-09-17 00:45:45 +0530

    add new logo and TextWorj project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -55,7 +55,7 @@ function Projects() {
       <Particle />
       <Container>
         <h1 className="project-heading">
-          My  <strong className="purple">Works </strong>
+          My  <strong className="purple">Projects</strong>
         </h1>
         <p style={{ color: "white" }}>
           Here are a few projects I've worked on recently.
